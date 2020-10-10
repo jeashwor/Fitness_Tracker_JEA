180363f18318a0b730fe3aaa4da4938cd74673a7
Jacob Ashworth <jeashwor@gmail.com>
2020-10-10 00:22:52 -0400

    server.js updates to try and fix heroku deployment issues

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const db = require("./models");
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -16,7 +17,7 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -25,6 +26,6 @@ mongoose.connect(
   }
 );
 
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log(`Now listening on: http://localhost:${PORT}`);
 });
