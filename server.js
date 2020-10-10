3aa7d00a0d8dc5f0e7679bd02446a2394b2cbdef
Jacob Ashworth <jeashwor@gmail.com>
2020-10-10 00:24:30 -0400

    another attemp

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const db = require("./models");
+const Workout = require("./models/Workout");
 
 const PORT = process.env.PORT || 8080;
 
