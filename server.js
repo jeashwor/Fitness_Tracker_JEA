bac32787d691f27175644568f3ea58a90e5343ce
Jacob Ashworth <jeashwor@gmail.com>
2020-10-09 23:20:14 -0400

    updated server.js with code to connect heroku app to mongoDB

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ require("./routes/htmlRoutes")(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
